cdc5a01561c33c2ebefaf8d0125617d8351b1dde
Sebastiao-Domingos <domingossebastiao380@gmail.com>
2024-06-25 22:08:55 +0100

    first

--- index.py
+++ index.py
@@ -1,2 +1,16 @@
 
-print("ola")
+
+
+class MyClass :
+
+    def sms(self):
+        print("ola POO")
+
+    def soma( self , num: int, num2 : int):
+        return num + num2
+
+myclass = MyClass()
+
+myclass.sms()
+
+print( myclass.soma(2,56))
